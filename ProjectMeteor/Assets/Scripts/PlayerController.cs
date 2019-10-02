74a617a2c7fd718e234e85a0caa35364f0536053
eychow <emilycc204@gmail.com>
2019-10-01 20:19:08 -0700

    [Emily] Switched player capsule to character model and animator

--- ProjectMeteor/Assets/Scripts/PlayerController.cs
+++ ProjectMeteor/Assets/Scripts/PlayerController.cs
@@ -176,6 +176,7 @@ public class PlayerController : MonoBehaviour
                 }
                 break;
         }
+        UpdateAnimations();
     }
 
     private void OnCollisionEnter(Collision collision)
