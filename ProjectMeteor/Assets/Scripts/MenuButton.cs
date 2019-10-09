be88933936c88f616a0a27977f2ba4f01b5eb7ae
William Lore <wlore322@gmail.com>
2019-10-08 18:02:38 -0700

    Main menu placeholder

---
+++ ProjectMeteor/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuButton : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorFunctions animatorFunctions;
+	[SerializeField] int thisIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(menuButtonController.index == thisIndex)
+		{
+			animator.SetBool ("selected", true);
+			if(Input.GetAxis ("Submit") == 1){
+				animator.SetBool ("pressed", true);
+                //
+                if (menuButtonController.index == 0)
+                {
+                    SceneManager.LoadScene("Level 1");
+                }
+                if (menuButtonController.index == 1)
+                {
+                    SceneManager.LoadScene("Cutscene");
+                }
+            }
+            else if (animator.GetBool ("pressed")){
+				animator.SetBool ("pressed", false);
+				animatorFunctions.disableOnce = true;
+			}
+		}else{
+			animator.SetBool ("selected", false);
+		}
+    }
+}
