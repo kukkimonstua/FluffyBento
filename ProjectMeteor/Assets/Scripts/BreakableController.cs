c8526e1a6265bf64e454ba082a372bfdaa366f49
sjandybae <sjandybae@gmail.com>
2019-10-08 21:22:16 -0700

    [Andy] Forgot to include the new assets, lol, PULL THIS TOO

---
+++ ProjectMeteor/Assets/Scripts/BreakableController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool isBroken;
+    private BoxCollider myCollider;
+    public Material testmat;
+
+    void Start()
+    {
+        myCollider = GetComponent<BoxCollider>();
+        isBroken = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetBroken()
+    {
+        if (!isBroken)
+        {
+            isBroken = true;
+            GetComponent<MeshRenderer>().material = testmat;
+            myCollider.center = new Vector3(myCollider.center.x, myCollider.size.y * -0.4f, myCollider.center.z);
+            myCollider.size = new Vector3(myCollider.size.x, myCollider.size.y * 0.1f, myCollider.size.z);
+        }
+    }
+    public void GetRestored()
+    {
+        if (isBroken)
+        {
+            isBroken = false;
+            GetComponent<MeshRenderer>().material = testmat;
+            myCollider.center = new Vector3(myCollider.center.x, 0.0f, myCollider.center.z);
+            myCollider.size = new Vector3(myCollider.size.x, myCollider.size.y * 10.0f, myCollider.size.z);
+        }
+    }
+}
---
+++ ProjectMeteor/Assets/Scripts/TextClamp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextClamp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text textToClamp;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 textPos = Camera.main.WorldToScreenPoint(transform.position);
+        textToClamp.transform.position = textPos;
+    }
+}
