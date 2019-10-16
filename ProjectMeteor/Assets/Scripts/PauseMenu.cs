78eb070d0b3e26f20927f5a2c6453185ec6ffc25
William Lore <wlore322@gmail.com>
2019-10-16 13:40:07 -0700

    - added PauseMenu UI element to canvas prefab.+ PauseMenu script - added conditional under UpdateAnimations() of PlayerController to stop the model from being able to rotate while game is paused - make sure EventSystem is linked to the PauseMenu script under PauseMenu (canvas ui element) and PauseMenu is linked under the Player prefab (LINKS TO GUI)
    Pause menu should freeze the game, highlight an option as default, and allow keyboard/controller navigation + selection to, for instance, switch scenes.

---
+++ ProjectMeteor/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public Button btn;
+    public EventSystem es;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+
+        //highlight initial menu option
+        es.SetSelectedGameObject(null);
+        es.SetSelectedGameObject(btn.gameObject);
+
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    public bool Paused()
+    {
+        return GameIsPaused;
+    }
+   
+}
