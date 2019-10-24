951f2795f383871ffe34849e1c2bddbcc449c679
William Lore <wlore322@gmail.com>
2019-10-24 06:01:38 -0700

    First iteration dialogue system using queue to output dialogue + profiles stored in scriptable objects

---
+++ ProjectMeteor/Assets/Scripts/DialogueTestScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DialogueTestScript : MonoBehaviour
+{
+    public DialogueBase dialogue;
+    public GameObject lastSelect;
+
+    //
+    void Start()
+    {
+        lastSelect = new GameObject();
+        TriggerDialogue();
+    }
+
+    public void TriggerDialogue()
+    {
+        DialogueManager.instance.EnqueueDialogue(dialogue);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            TriggerDialogue();
+        }
+
+        //reset selection to continue button for non-button clicks
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelect);
+        }
+        else
+        {
+            lastSelect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+
+
+}
