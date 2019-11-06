1d9bcd0567e8bc765bf1eaa258d43a41017ca66f
sjandybae <sjandybae@gmail.com>
2019-11-06 14:52:34 -0800

    [Andy] Added dual sprite support to cutscene, condensed into a prefab

--- ProjectMeteor/Assets/Scripts/DialogueBase.cs
+++ ProjectMeteor/Assets/Scripts/DialogueBase.cs
@@ -10,6 +10,7 @@ public class DialogueBase : ScriptableObject
     {
         public string myName;
         public Sprite portrait;
+        public bool isLeftPortrait;
         [TextArea(4,8)]
         public string myText;
     }
