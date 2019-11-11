165dd32192c730b22ff9f9e92c86bc5f5d08092e
sjandybae <sjandybae@gmail.com>
2019-11-10 20:04:28 -0800

    [Andy] Adjusted a few things and added some missing files

--- ProjectMeteor/Assets/Scripts/GameManager.cs
+++ ProjectMeteor/Assets/Scripts/GameManager.cs
@@ -6,12 +6,17 @@ public class GameManager : MonoBehaviour
 {
     //THIS IS A STATIC MASTER CLASS, HOPEFULLY
     public const int MAIN_MENU_INDEX = 0;
-    public const int LEVEL_1_OP = 1;
-    public const int LEVEL_1_ED = 2;
-    public const int LEVEL_2_OP = 3;
-    public const int LEVEL_2_ED = 4;
-    public const int LEVEL_3_OP = 5;
-    public const int LEVEL_3_ED = 6;
+    public const int LEVEL_1 = 1;
+    public const int LEVEL_2 = 2;
+    public const int LEVEL_3 = 3;
+    public const int SURVIVAL_MODE = 4;
+
+    public const int LEVEL_1_OP = 5;
+    public const int LEVEL_1_ED = 6;
+    public const int LEVEL_2_OP = 7;
+    public const int LEVEL_2_ED = 8;
+    public const int LEVEL_3_OP = 9;
+    public const int LEVEL_3_ED = 10;
 
     public static int sceneIndex;
     
