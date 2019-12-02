7907fdac7f30a0712c6379184729277dd3a59be2
sjandybae <sjandybae@gmail.com>
2019-12-01 22:36:52 -0800

    Adjusted PlatformAttach script to require less manual setup

--- ProjectMeteor/Assets/Scripts/PlatformAttach.cs
+++ ProjectMeteor/Assets/Scripts/PlatformAttach.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class PlatformAttach : MonoBehaviour
 {
-    public GameObject Player;
+    //public GameObject Player;
 
     private void OnTriggerEnter (Collider other)
     {
-        if (other.gameObject == Player)
+        if (other.gameObject.GetComponent<PlayerController>() != null)
         {
-            Player.transform.parent = transform;
+            other.transform.parent = transform;
         }
     }
 
     private void OnTriggerExit (Collider other)
     {
-        if (other.gameObject == Player)
+        if (other.gameObject.GetComponent<PlayerController>() != null)
         {
-            Player.transform.parent = null;
+            other.transform.parent = null;
         }
     }
 }
